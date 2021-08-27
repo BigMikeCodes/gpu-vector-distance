c088b94d2252c779e18de81663bdc52f7ff9c973
Michael Farrant <michaelfarrant@outlook.com>
2021-08-27 16:38:17 +0100

    Alted the main method to load a file that contains vecotrs defined as rows of CSVs

--- main.c
+++ main.c
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+int CHAR_BUFFER_SIZE = 1024;
+
 int main(int argc, char *argv[])
 {
-    printf("Hello world");
+    
+    FILE *inputVectorFile = fopen("vectors.csv","r");
+
+    char lineBuffer[CHAR_BUFFER_SIZE];
+
+    while (fgets(lineBuffer, CHAR_BUFFER_SIZE, inputVectorFile))
+    {
+        printf("%s",lineBuffer);
+    }
+    
+    fclose(inputVectorFile);
 
     return EXIT_SUCCESS;
 }
